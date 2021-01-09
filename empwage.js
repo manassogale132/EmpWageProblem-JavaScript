0acb75d372b5271495f3c51857b0af2e0a122b58
manassogale132 <manassogale@gmail.com>
2021-01-09 14:56:28 +0530

    first commit

---
+++ empwage.js
@@ -0,0 +1,13 @@
+console.log("\n----------------------------------------------------------\n"+
+            "--Welcome to Employee Wage Computation Problem--\n"+
+            "----------------------------------------------------------\n");
+
+
+var attendance = Math.floor(Math.random() * 2) + 1;                  //Attendance check
+
+if (attendance == 1) {
+  console.log("Employee is Absent");
+} else {
+  console.log("Employee is Present");
+}
+                                                
