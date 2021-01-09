fdfecbd951e7120bd379c7a46528bf40d511cad9
manassogale132 <manassogale@gmail.com>
2021-01-09 15:04:01 +0530

    second commit.

--- empwage.js
+++ empwage.js
@@ -2,6 +2,11 @@ console.log("\n----------------------------------------------------------\n"+
             "--Welcome to Employee Wage Computation Problem--\n"+
             "----------------------------------------------------------\n");
 
+const fullDayHour = 8;                                              
+const wagePerHour = 20;
+var fullWage = fullDayHour * wagePerHour;                      //Calculate daily employee wage  (full-wage)
+
+console.log("Full Employee Wage is : " + fullWage);
 
 var attendance = Math.floor(Math.random() * 2) + 1;                  //Attendance check
 
