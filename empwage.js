0d15e750791c50526d441251b9e42489d2b80854
manassogale132 <manassogale@gmail.com>
2021-01-09 17:06:20 +0530

    sixth commit.

--- empwage.js
+++ empwage.js
@@ -4,37 +4,54 @@ console.log("\n----------------------------------------------------------\n"+
 
 const fullDayHour = 8;     
 const partTimeHour = 4;                                            
-const wagePerHour = 20;
-const workingDaysPerMonth = 20;                               
+const wagePerHour = 20;    
+const workingHoursPerMonth = 100;     
+const workingDaysPerMonth = 20;        
+
 var fullWage = fullDayHour * wagePerHour;                      //Calculate daily employee wage  (fullWage)
 var partTimeWage = partTimeHour * wagePerHour;                 //Calculate parttime wage added  (partTimeWage)
 
+var sum = 0;
+var workHour = 0;
+var workDay = 1;
 
-for (var i = 1; i <= workingDaysPerMonth; i++) {               //Calculating Wages for a Month (workingDaysPerMonth)
+while(workHour <= workingHoursPerMonth && workDay <= workingDaysPerMonth) {   //Two Condition To be Checked  
 
   var attendance = Math.floor(Math.random() * 3) + 1;       //Attendance check
 
   switch (attendance) {                                                       //Solved using switch case statement 
     
-    case 1:
-      console.log("Day no : " + i);
-      console.log("Employee is Absent\n");
+    case 1 :
+      console.log("Day no : " + workDay + "\n" +
+                  "Employee is Absent.\n"+
+                  ".....");
       break;
 
-    case 2:
-      console.log("Day no : " + i);
-      console.log("Employee is Parttime");
-      console.log("PartTime Employee Wage is : " + partTimeWage + "\n");
+    case 2 :
+      sum = sum + partTimeWage;
+      workHour = workHour + partTimeHour;
+      console.log("Day no : " + workDay + "\n" +
+                  "Hour no : " + workHour + "\n" +
+                  "Employee is Parttime." + "\n" +
+                  "PartTime Employee Wage is : " + partTimeWage + "\n" +
+                  "Total Wage is : " + sum + "\n" +
+                  ".....");
       break;
 
-    case 3:
-      console.log("Day no : " + i);
-      console.log("Employee is Present");
-      console.log("Full Employee Wage is : " + fullWage + "\n");
+    case 3 :
+      sum = sum + fullWage;
+      workHour = workHour + fullDayHour;
+      console.log("Day no : " + workDay + "\n" +
+                  "Hour no : " + workHour + "\n" +
+                  "Employee is Present." + "\n" +
+                  "Full Employee Wage is : " + fullWage + "\n" +
+                  "Total Wage is : " + sum + "\n" +
+                  ".....")
       break;
 
     default:
       console.log("No such Case");
       break;
   }
+  workDay++;
 }
