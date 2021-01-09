2c36714b6644aab719ed180ef3df020bc0194ad8
manassogale132 <manassogale@gmail.com>
2021-01-09 15:10:19 +0530

    third commit.

--- empwage.js
+++ empwage.js
@@ -2,17 +2,23 @@ console.log("\n----------------------------------------------------------\n"+
             "--Welcome to Employee Wage Computation Problem--\n"+
             "----------------------------------------------------------\n");
 
-const fullDayHour = 8;                                              
+const fullDayHour = 8;     
+const partTimeHour = 4;                                            
 const wagePerHour = 20;
 var fullWage = fullDayHour * wagePerHour;                      //Calculate daily employee wage  (full-wage)
+var partTimeWage = partTimeHour * wagePerHour;                 //Calculate parttime wage added  (partime-wage)
+
 
 console.log("Full Employee Wage is : " + fullWage);
+console.log("PartTime Employee Wage is : " + partTimeWage);
 
-var attendance = Math.floor(Math.random() * 2) + 1;                  //Attendance check
+var attendance = Math.floor(Math.random() * 3) + 1;                  //Attendance check
 
 if (attendance == 1) {
-  console.log("Employee is Absent");
-} else {
-  console.log("Employee is Present");
-}
-                                                
+    console.log("Employee is Absent");
+  } else if (attendance == 2) {
+    console.log("Employee is Parttime");                            //Parttime employee added
+  } else {
+    console.log("Employee is Present");
+  }
+                
